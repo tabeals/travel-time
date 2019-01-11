1406fdf2ebf74f1015ac919a10ea8be5145ae605
Thomas Albert Beals Jr <tabeals@outlook.com>
2019-01-11 15:41:36 -0500

    these are the travel time files for my portfolio.

---
+++ js/destination.js
@@ -0,0 +1,225 @@
+// JavaScript Document
+$(document).ready(function(){
+	/*destination page animations-----------*/
+	$('#fade').hide();
+	$('#fade').fadeIn(3000);
+	
+
+
+	$('#tropicalUla').hide();
+	$('#tropicalUla').fadeIn(3000);
+	
+
+
+	$('#tropicalUlb').hide();
+	$('#tropicalUlb').fadeIn(3000);
+	
+/*drop down menu---------------------------*/
+	$('#navDestinations').hide();
+	
+	$('#destination').on('mouseover', function(){
+		$('#navDestinations').show();
+	});
+	
+
+	$('#navDestinations').on('mouseover',function(){
+		$('#navDestinations').show();
+	});
+	$('#navDestinations').on('mouseout',function(){
+		$('#navDestinations').hide();
+	});
+/*Activities Page---------------------------*/
+
+/*atv-----------*/	
+$('#atv').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#atv').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/atv.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+});
+/*bikes-----------*/	
+$('#bikes').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur.');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#bikes').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/bikes.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+});
+/*catamaran-----------*/
+$('#catamaran').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#catamaran').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/catamaran.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+});
+/*fishing-----------*/
+$('#fishing').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#fishing').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/fishing.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+});
+	
+/*hiking-----------*/
+$('#hiking').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur.');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#hiking').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/hiking.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+});
+/*horseback-----------*/
+$('#horseback').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#horseback').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/horseback.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+});
+/*hunting-----------*/
+$('#hunting').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#hunting').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/hunting.jpeg" >');
+	
+}, function(){
+	$('#bottom').html('');
+	});
+/*jetski-----------*/
+$('#jetski').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur.');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#jetski').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/jetski.jpeg">');
+
+	
+}, function(){
+	$('#bottom').html('');
+	});
+/*scuba-----------*/
+$('#scuba').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#scuba').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/scuba.jpeg" alt="scuba">');
+	
+}, function(){
+	$('#bottom').html('');
+	});
+/*ski-----------*/
+	$('#ski').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#ski').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/ski.jpeg">');
+	
+}, function(){
+	$('#bottom').html('');
+});
+/*snowmobile-----------*/
+	$('#snowmobile').hover(function(){
+	$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur. ');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#snowmobile').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/snowmobile.jpeg">');
+	
+}, function(){
+	$('#bottom').html('');
+});
+
+/*waterski-----------*/
+$('#waterski').hover(function(){
+$('#top').text('	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt aliquid asperiores amet quia mollitia possimus dolorem adipisci cum excepturi quidem quasi veniam quis fugit, laudantium, delectus fuga dolor iusto consequatur.');
+	
+}, function(){
+	$('#top').text('');
+});
+
+$('#waterski').hover(function(){
+	$('#bottom').html('<img width="440px" height="210px" src="../images/avtivities images/waterski.jpeg">');
+	
+}, function(){
+	$('#bottom').html('');
+});
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
